9a44fb27a3bf4175aaf150609de2e993eb001277
Fair & Square Synthesis <thom@sqrsy.com>
2024-12-31 16:03:02 +1100

    Add support for trigger signals

--- rasa6-do/rapid_trigger/digitalWrite_at_fixed_interval.h
+++ rasa6-do/rapid_trigger/digitalWrite_at_fixed_interval.h
@@ -1,5 +1,5 @@
 int initial_time = 0;
-bool digitalWrite_at_fixed_interval(int PIN_OUT, int millisecond_interval){
+bool digitalWrite_at_fixed_interval(int PIN_OUT, int millisecond_interval) {
 
   // by default, this function will return false
   bool cycle_finished = false;
@@ -9,17 +9,17 @@ bool digitalWrite_at_fixed_interval(int PIN_OUT, int millisecond_interval){
   int diff = current_time - initial_time;
 
   // once [2*interval] time passes, reset timer
-  if(diff <= millisecond_interval){
+  if (diff <= millisecond_interval) {
     digitalWrite(PIN_OUT, 1);
-  }else{
-    if(diff <= 2*millisecond_interval){
+  } else {
+    if (diff <= 2 * millisecond_interval) {
       digitalWrite(PIN_OUT, 0);
-    }else{
+    } else {
       digitalWrite(PIN_OUT, 0);
       initial_time = millis();
       cycle_finished = true;
     }
   }
-  
-  return(cycle_finished);
+
+  return (cycle_finished);
 }
--- rasa6-do/rapid_trigger/rapid_trigger.ino
+++ rasa6-do/rapid_trigger/rapid_trigger.ino
@@ -30,9 +30,9 @@ int triggers_available = 0;
 bool debug = false;
 
 void setup() {
-  
+
   // set up all PINS for the chip
-  if(debug) Serial.begin(9600);
+  if (debug) Serial.begin(9600);
   pinMode(PIN_CLKIN, INPUT);
   pinMode(PIN_POT1, INPUT);
   pinMode(PIN_POT2, INPUT);
@@ -51,20 +51,20 @@ void loop() {
 
   // convert first pot voltage into integer range 0-7 => map to 1-8
   pot1_mV = read_analog_mV(PIN_POT1, 0, 0);
-  number_of_triggers = mV_to_integer(pot1_mV, 7, 1, 5); // integer ranges 0-7
+  number_of_triggers = mV_to_integer(pot1_mV, 7, 1, 5);  // integer ranges 0-7
   number_of_triggers = 8 - number_of_triggers;
-  if(debug){
-    Serial.print ("Pot 1 value: ");
-    Serial.println (number_of_triggers);
+  if (debug) {
+    Serial.print("Pot 1 value: ");
+    Serial.println(number_of_triggers);
   }
 
   // convert second pot voltage into integer range 0-7 => map to 10-80
   pot2_mV = read_analog_mV(PIN_POT2, 0, 0);
-  trigger_duration_ms = mV_to_integer(pot2_mV, 7, 1, 5); // integer ranges 0-7
-  trigger_duration_ms = 10 * (8-trigger_duration_ms);
-  if(debug){
-    Serial.print ("Pot 2 value: ");
-    Serial.println (trigger_duration_ms);
+  trigger_duration_ms = mV_to_integer(pot2_mV, 7, 1, 5);  // integer ranges 0-7
+  trigger_duration_ms = 10 * (8 - trigger_duration_ms);
+  if (debug) {
+    Serial.print("Pot 2 value: ");
+    Serial.println(trigger_duration_ms);
   }
 
   //////////////////////////////////////////////////////////////////
@@ -73,19 +73,19 @@ void loop() {
 
   // only run once per clock cycle
   // run on rising edge of incoming clock signal
-  if(current_clk & clk_reset){
+  if (current_clk & clk_reset) {
 
     // reset the rapid fire counter
     // there are Y rapid fire triggers per event
     triggers_available = number_of_triggers;
 
-    if(debug) Serial.println ("Advance clock.");
+    if (debug) Serial.println("Advance clock.");
     clk_reset = false;
   }
 
   // only run once per clock cycle
   // run on falling edge of incoming clock signal
-  if(!current_clk & !clk_reset){
+  if (!current_clk & !clk_reset) {
     clk_reset = true;
   }
 
@@ -93,37 +93,28 @@ void loop() {
   ///// WRITE OUTPUT
   //////////////////////////////////////////////////////////////////
 
-  // write rapid fire triggers while clock is TRUE
-  if(current_clk){
+  // when triggered by clock rise:
+  // - write and decrement triggers until none are available in pool
+  // - each trigger writes X/2 ms HIGH followed by X/2 ms LOW
+  // - cycle_finished = true after X ms has passed
+  if (triggers_available > -1) {  // one trigger is spent to set up the cycle_finished switch
 
-    // write and decrement triggers until none are available in pool
-    // each trigger writes X/2 ms HIGH followed by X/2 ms LOW
-    // cycle_finished = true after X ms has passed
-    if(triggers_available > -1){ // one trigger is spent to set up the cycle_finished switch
-      
-      digitalWrite(PIN_OUT2, 1); // the second output sends TRUE while rapid fire trigger occurs
-      cycle_finished = digitalWrite_at_fixed_interval(PIN_OUT1, trigger_duration_ms);
-      if(cycle_finished){
-        triggers_available = triggers_available - 1;
-        if(debug){
-          Serial.print ("Triggers available: ");
-          Serial.println (triggers_available);
-        }
+    digitalWrite(PIN_OUT2, 1);  // the second output sends TRUE while rapid fire trigger occurs
+    cycle_finished = digitalWrite_at_fixed_interval(PIN_OUT1, trigger_duration_ms);
+    if (cycle_finished) {
+      triggers_available = triggers_available - 1;
+      if (debug) {
+        Serial.print("Triggers available: ");
+        Serial.println(triggers_available);
       }
-      
-    }else{
-
-      // disable all output
-      digitalWrite(PIN_OUT1, 0);
-      digitalWrite(PIN_OUT2, 0);
     }
 
-  }else{
+  } else {
 
     // disable all output
     digitalWrite(PIN_OUT1, 0);
     digitalWrite(PIN_OUT2, 0);
   }
 
-  if(debug) delay(250);
+  if (debug) delay(250);
 }
